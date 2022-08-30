b7c313b74882889c02945b61cc19c8d6e672a25f
Natalia Homusko <avgust_net85@mail.ru>
2022-08-30 22:44:49 +0300

    lesson-2

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+import {timer} from './modules/timer.js'
+
+const init = () => {
+    timer('2022/08/30 22:28'); 
+}
+
+init();
---
+++ scripts/modules/timer.js
@@ -0,0 +1,57 @@
+export const timer = (deadline) => {
+    const timerCountDays = document.querySelector('.timer__count_days')
+    const timerCountHours = document.querySelector('.timer__count_hours')
+    const timerCountMinutes = document.querySelector('.timer__count_minutes')
+    document.querySelector('.timer').setAttribute('data-timer-deadline', deadline);
+
+
+    const getTimeRemaining = () => {
+        const dateStop = new Date(deadline).getTime();
+        const dateNow = Date.now();
+        const timeRemaining = dateStop - dateNow;
+
+        const minutes = Math.floor(timeRemaining / 1000 / 60 % 60);
+        const hours = Math.floor(timeRemaining / 1000 / 60 / 60 % 24);
+        const days = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
+
+        return { timeRemaining, minutes, hours, days }
+    }
+
+    const start = () => {
+        const timer = getTimeRemaining();
+
+        const getNoun = (number, one, two, five) => {
+            let n = Math.abs(number);
+            n %= 100;
+            if (n >= 5 && n <= 20) {
+                return five;
+            }
+            n %= 10;
+            if (n === 1) {
+                return one;
+            }
+            if (n >= 2 && n <= 4) {
+                return two;
+            }
+            return five;
+        }
+
+        timerCountDays.textContent = timer.days;
+        timerCountDays.nextElementSibling.textContent = getNoun(timer.days, 'день', 'дня', 'дней')
+        timerCountHours.textContent = timer.hours;
+        timerCountHours.nextElementSibling.textContent = getNoun(timer.hours, 'час', 'часа', 'часов')
+        timerCountMinutes.textContent = timer.minutes;
+        timerCountMinutes.nextElementSibling.textContent = getNoun(timer.minutes, 'минута', 'минуты', 'минут')
+
+        const intervalId = setTimeout(start, 1000 * 60);
+
+        if (timer.timeRemaining <= 0) {
+            clearTimeout(intervalId);
+            document.querySelector('.hero__text').style.display = "none";
+            document.querySelector('.hero__timer').style.display = "none";
+        }
+    }
+
+    start();
+};
+
