97603dba9a67b50df696036e5b76378832bbb6f4
Natalia Homusko <avgust_net85@mail.ru>
2022-09-02 14:30:03 +0300

    fix burger

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,7 @@
 import {timer} from './modules/timer.js';
 import './modules/acc.js';
-//import './modules/burger.js';
+import './modules/burger.js';
 
-
-const button = document.querySelector('.header__menu-button');
-const menuWrapper = document.querySelector('.header__menu');
-
-button.addEventListener('click', () => {
-    menuWrapper.classList.toggle('header__menu_active');
-}) 
 
 const init = () => {
     if (document.body.contains(document.querySelector("*[data-timer-deadline]"))) {
--- scripts/modules/burger.js
+++ scripts/modules/burger.js
@@ -1,6 +1,27 @@
 const button = document.querySelector('.header__menu-button');
-const menuWrapper = document.querySelector('.header__menu');
+const menu = document.querySelector('.header__menu');
+const menuItem = document.querySelectorAll(".header__item");
+
+const menuToggle = () => {
+    menu.classList.toggle('header__menu_active');
+};
 
 button.addEventListener('click', () => {
-    menuWrapper.classList.toggle('header__menu_active');
+    menuToggle();
 }); 
+
+menuItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if(menu.classList.contains('header__menu_active')) {
+            menuToggle();
+        } 
+    });
+});
+
+document.addEventListener("click", function (e) {
+    const target = e.target;
+    if (target !== menu && target !== button && 
+        menu.classList.contains('header__menu_active')) {
+        menuToggle();
+    }
+});
